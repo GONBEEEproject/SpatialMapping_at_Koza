71f60ae8d366647e4106b73aac64073df01c1c87
DESKTOP-PCRS882\gonbe <gonbeee.project@gmail.com>
2018-08-30 21:54:07 +0900

    First and Last Commit

---
+++ Assets/Scripts/WorldCursor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCursor : MonoBehaviour {
+
+    private MeshRenderer meshRenderer;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var headPos = Camera.main.transform.position;
+        var gazeDir = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(headPos,gazeDir,out hitInfo))
+        {
+            meshRenderer.enabled = true;
+
+            transform.position = hitInfo.point;
+
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+        else
+        {
+            meshRenderer.enabled = false;
+        }
+
+
+
+
+
+
+	}
+}
